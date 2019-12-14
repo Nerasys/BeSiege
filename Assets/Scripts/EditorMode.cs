ad28de8494a6162e37d402ee1acbe7708b09df59
Frappez Sylvain <sylvain.frappez@gmail.com>
2019-12-14 12:13:41 +0100

    {Init Project}

---
+++ Assets/Scripts/EditorMode.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorMode : MonoBehaviour
+{
+    [SerializeField] Material mat;
+    [SerializeField] GameObject construction;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            if (hit.collider.gameObject.GetComponent<Constructable>())
+            {
+                
+
+            }
+
+        }
+    }
+}
