9af25d858178079e09b9789b48247853dd3387ea
Nerasys <sylvain.frappez@gmail.com>
2019-12-14 16:55:00 +0100

    {EDITOR MODE}

--- Assets/Scripts/EditorMode.cs
+++ Assets/Scripts/EditorMode.cs
@@ -6,14 +6,16 @@ public class EditorMode : MonoBehaviour
 {
     [SerializeField] Material mat;
     [SerializeField] GameObject construction;
-
-
+    [SerializeField] GameObject vehicule;
+    bool IsBuilding = false;
+    GameObject gameObjectBuild;
+    GameObject collisionSet;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -26,10 +28,43 @@ public class EditorMode : MonoBehaviour
         {
             if (hit.collider.gameObject.GetComponent<Constructable>())
             {
+              
+                if (!IsBuilding)
+                {
+                    gameObjectBuild = Instantiate(construction);
+                    gameObjectBuild.GetComponent<MeshRenderer>().material = mat;
+                    gameObjectBuild.transform.SetParent(vehicule.transform);
+                    gameObjectBuild.transform.position = hit.collider.gameObject.transform.position;
                 
+               
+                    
+                    IsBuilding = true;
+                    collisionSet = hit.collider.gameObject;
+                }
+                else
+                {
+                    gameObjectBuild.transform.position = hit.collider.gameObject.transform.position;
+                    collisionSet = hit.collider.gameObject;
+                }
 
+                float dot = Vector3.Dot(gameObjectBuild.transform.forward, hit.normal);
+                Debug.Log(dot);
             }
-
         }
+
+
+
+        if (IsBuilding && Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(collisionSet.name);
+            Destroy(collisionSet);
+            IsBuilding = false;
+        }
+
+
     }
+
 }
+
+
+
